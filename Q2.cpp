7b1dded53a02c856a8d793f3e0c72690fdfe6ba6
Sonu ranabhat <sonu.ranabhat64@gmail.com>
2021-01-05 15:23:48 +0545

    version2

--- Q2.cpp
+++ Q2.cpp
@@ -1,3 +1,9 @@
+/*Q2.Replace all vowels in the sentence with the next closest vowel in the sentence itself. 
+The last vowel should be replaced by the first vowel in the sentence.
+Example
+Sample Input 1: Hello World
+Sample output 1 Hollo Werld
+*/ 
 #include<bits/stdc++.h>
 using namespace std;
 int main()
@@ -5,14 +11,13 @@ int main()
 string s,v;
 getline(cin,s);
 int n=s.size();
-int j,k;
-
+int j=0;
+ 
 //for storing vowel in v[j] from string s
 for (int i=0;i<n;i++)
 {
 if(s[i]=='a'||s[i]=='e'||s[i]=='i'||s[i]=='o'||s[i]=='u')
-v[j++]=s[i];
-k=j;
+v+=s[i];
 }
 // replacing last vowel with the first vowel
 for (int i=n-1;i>=0;i--)
@@ -20,14 +25,12 @@ for (int i=n-1;i>=0;i--)
 if(s[i]=='a'||s[i]=='e'||s[i]=='i'||s[i]=='o'||s[i]=='u')
 {
 s[i]=v[0];
-j=0;
+n=i;;
 break;
 }
 }
 //shifting the next vowel found in s[i]
 for (int i=0;i<n;i++)
-{
-if((j+1)<k)
 {
 if(s[i]=='a'||s[i]=='e'||s[i]=='i'||s[i]=='o'||s[i]=='u')
 {
@@ -35,8 +38,7 @@ s[i]=v[j+1];
 j++;
 }
 }
-}
-cout<<s;
-
-
+cout<<s<<endl;
+cout<<v;
+ 
 }
