cdf49d5e414c69fdbd770c7227c03044355bfa80
Sonu ranabhat <sonu.ranabhat64@gmail.com>
2021-01-05 15:35:06 +0545

    version4

--- Q3.cpp
+++ Q3.cpp
@@ -1,36 +1,89 @@
+/*
+Sample Input 1
+Take what you can, give nothing back.
+Sample Output 1:
+akeTarg hatwarg ouyarg ancarg, ivegarg othingnarg ackbarg
+Note:
+• Punctuations should remain at the end of the word even after translation. Assume that punctuations won't appear other than end of the word. Punctuations to be considered are ....?!
+. There could be multiple punctuations present (eg: Yes!!)
+//non continuous single special charater
+Take? what! you can, give nothing back. 
+akeTarg? hatwarg! ouyarg ancarg, ivegarg othingnarg ackbarg.
+*/
+//non continuous single special charater
 #include<iostream>
-#include<string>
-#include<bits/stdc++.h>
 using namespace std;
 int main()
-{
-	string  s,s1,s2,s3=" ";
+{ 
+	string s,s1,s2,temp;
+	int flag=0;
 	char c=' ';
 	getline(cin,s);
 	int n=s.size();
+	
 	for(int i=0;i<n;i++) 
 	{
-		if(s[i]!=c)
+		if((s[i]!=c))
 		{
 			s1+=s[i];
 		}
 		else
 		{
-			s1+=s3;
-			c=s1[0];
-			s1.erase(s1.begin());
+		c=s1[0];
+		s1.erase(s1.begin());
+		int m=s1.size();	
+		for(int j=0;j<m;j++)
+		{
+		if(s1[j]==','||s1[j]=='?'||s1[j]==';'||s1[j]=='!')
+			{
+			temp=s1[j];
+			s1.erase(s1.begin()+j);
+			s1+=c;
+			flag++;
+			break;			
+			}
+		}
+		if(flag!=0)
+		{
+			s2+=(s1+"arg"+temp+" ");
+			s1.clear();
+			c=' ';
+			flag=0;
+		}
+		else
+		{
 			s1+=c;
 			s2+=(s1+"arg ");
 			s1.clear();
 			c=' ';
+			flag=0;
+		}
 		}
 	}
+		c=s1[0];
+		s1.erase(s1.begin());
+		int m=s1.size();	
+		for(int j=0;j<m;j++)
+		{
+		if(s1[j]==','||s1[j]=='?'||s1[j]==';'||s1[j]=='!')
+			{
+			temp=s1[j];
+			s1.erase(s1.begin()+j);
+			s1+=c;
+			flag++;
+			break;			
+			}
+		}
+		if(flag!=0)
+		{
+			s2+=(s1+"arg"+temp+" ");
+		}
+		else
+		{
+			s1+=c;
+			s2+=(s1+"arg ");
+		}
 	
-	char c1=s1[0];
-	s1.erase(s1.begin());
-	s1+=s3;
-	s1+=c1;
-	s2+=(s1+"arg ");
-	cout<<s2;
+	cout<<s2<<endl;
 	
 }
