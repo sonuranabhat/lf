49b9b7de2f13c46cc5e22840b513f00e8e81515c
Sonu ranabhat <sonu.ranabhat64@gmail.com>
2021-01-05 15:17:22 +0545

    version2

--- Q1.cpp
+++ Q1.cpp
@@ -3,42 +3,41 @@
 //into the array. If the digits are separated by any character, then they are not considered adjacent.
 // Digits that don't have adjacent digits should be inserted into array as it is. If there are no digits 
 //present in the string. then the array should contain a single element - 0.
+//
 #include<iostream>
 #include<bits/stdc++.h>
 #include<string>
 using namespace std;
-#define charToInt(c)(c-'0')
 int main()
 {
-	string str;
-	cin>>str;
-	int n=str.size();
-	vector<int>v;
-	int temp=0;
-	for(int i=0;i<=n;i++)
-	{
-		if(isdigit(str[i]))
-		{			
-			int a=charToInt(str[i]);
-			temp=temp+a;
-
-		}
-		else
-		{
-			if(temp)
-			{
-				v.push_back(temp);
-				temp=0;
-			}
-		}
-
-	}
-	int n1=v.size();
-	for(int j=0;j<n1;j++)
-	{
-		cout<<v[j]<<" ";
-	}
-
-	
-	return 0;
+string str;
+cin>>str;
+int n=str.size();
+vector<int>v;
+int temp=0;
+for(int i=0;i<n+1;i++)
+{
+ if(isdigit(str[i]))
+ {
+  char x=str[i];
+  int p;
+  p=(int)x;
+  p=p-48;
+  temp=temp+p;
+ }
+ else
+ {
+  if(temp!=0)
+  {
+   v.push_back(temp);
+   temp=0;
+  }
+ }
+}
+int n1=v.size();
+for(int j=0;j<n1;j++)
+{
+ cout<<v[j]<<"";
+}
+return 0;
 }
