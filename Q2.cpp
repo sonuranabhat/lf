0159ef71c968b6278d2153548cc713e2104339ec
Sonu ranabhat <sonu.ranabhat64@gmail.com>
2021-01-05 15:31:12 +0545

    version3

--- Q2.cpp
+++ Q2.cpp
@@ -3,28 +3,47 @@ The last vowel should be replaced by the first vowel in the sentence.
 Example
 Sample Input 1: Hello World
 Sample output 1 Hollo Werld
-*/ 
-//its in reverse not shifting
-#include<iostream>
-#include<string>
+*/
 #include<bits/stdc++.h>
 using namespace std;
 int main()
 {
-string s,vowel;
+string s,v;
 getline(cin,s);
 int n=s.size();
-int j;
+int j,k;
+ 
+//for storing vowel in v[j] from string s
 for (int i=0;i<n;i++)
 {
- if(s[i]=='a'||s[i]=='e'||s[i]=='i'||s[i]=='o'||s[i]=='u')
- vowel[j++]=s[i];
+if(s[i]=='a'||s[i]=='e'||s[i]=='i'||s[i]=='o'||s[i]=='u')
+v[j++]=s[i];
+k=j;
 }
+// replacing last vowel with the first vowel
+for (int i=n-1;i>=0;i--)
+{
+if(s[i]=='a'||s[i]=='e'||s[i]=='i'||s[i]=='o'||s[i]=='u')
+{
+s[i]=v[0];
+j=0;
+break;
+}
+}
+//shifting the next vowel found in s[i]
 for (int i=0;i<n;i++)
 {
- if(s[i]=='a'||s[i]=='e'||s[i]=='i'||s[i]=='o'||s[i]=='u')
- s[i]=vowel[--j];
+if((j+1)<k)
+{
+if(s[i]=='a'||s[i]=='e'||s[i]=='i'||s[i]=='o'||s[i]=='u')
+{
+s[i]=v[j+1];
+j++;
+}
+}
 }
 cout<<s;
-
+ 
+ 
 }
+
